f0dd0ba5c07dbde6d22aab87f68e6df31d8ba8a7
Ramzi Kanazi <arcan3xx>
2016-07-31 17:05:29 -0400

    connect api

---
+++ joblog_app/app/controllers/jobs_controller.rb
@@ -0,0 +1,8 @@
+class JobsController < ApplicationController
+  def index
+    url = "https://data.cityofnewyork.us/resource/swhp-yxa4.json"
+    response = HTTParty.get(url)
+    parsed_body = JSON.parse(response.body)
+    render json: parsed_body
+  end
+end
--- joblog_app/config/routes.rb
+++ joblog_app/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  # get 'jobs/index'
+  resources :jobs
 
   resources :articles do
     resources :comments
