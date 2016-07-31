649c782aa036715bb9dcf1dcb6325c6c0bb11f90
Ramzi Kanazi <arcan3xx>
2016-07-30 23:00:46 -0400

    show the job posts

--- joblog_app/app/controllers/articles_controller.rb
+++ joblog_app/app/controllers/articles_controller.rb
@@ -1,4 +1,12 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
